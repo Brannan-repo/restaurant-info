778282db13ba63b4f28a5aea01c3b576cb3bc016
Brannan-repo <brannan.n@gmail.com>
2021-09-18 14:59:15 -0400

    Basic start

---
+++ src/main/java/com/brannan/yelp/reviews/restaurantinfo/pojos/Restaurant.java
@@ -0,0 +1,15 @@
+package com.brannan.yelp.reviews.restaurantinfo.pojos;
+
+public class Restaurant {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
--- src/main/java/com/brannan/yelp/reviews/restaurantinfo/rest/RestaurantInfoRestProvider.java
+++ src/main/java/com/brannan/yelp/reviews/restaurantinfo/rest/RestaurantInfoRestProvider.java
@@ -4,13 +4,16 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brannan.yelp.reviews.restaurantinfo.pojos.Restaurant;
 
 @RestController
 class RestaurantInfoRestProvider {
 
-	@GetMapping( value = "/get-reviews", produces = MediaType.APPLICATION_JSON_VALUE )
-	public String one() {
-		return null;
+	@GetMapping(value = "/get-reviews", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Restaurant getRestaurantById() {
+		Restaurant restaurant = new Restaurant();
+		restaurant.setId("AGbDnpKNHPcPBbbc9GycSw");
+		return restaurant;
 	}
 
 }
